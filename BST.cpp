f0fa76f8387256332ab8040012e9272c7efb4265
Yizhou Mi <miyizhou@remote.ecf.utoronto.ca>
2024-11-13 22:59:52 -0500

    start

---
+++ BST.cpp
@@ -0,0 +1,13 @@
+//
+//  BST.cpp
+//  Lab 5 Search Through A Database
+//
+//  Created by Joseph Mi, Date
+#include "BST.h"
+
+#include "Employee.h"
+
+BST::BST(string order_) {
+  root = NULL;
+  order = order_;
+}
